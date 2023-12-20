c311c414a27a3e0d4f9631a1ecb7bdc272d0468a
waynetipsy <jacketoniru@gmail.com>
2023-12-20 21:27:00 +0100

    functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "app.ify.myshoppinglistapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "app.ify.myshoppinglistapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/app/ify/myshoppinglistapp/ShoppingList.kt
@@ -0,0 +1,56 @@
+package app.ify.myshoppinglistapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+data class ShoppingItem(
+    val id:Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false ){
+
+}
+
+@Composable
+  fun ShoppingListApp () {
+    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ){
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text( "Add Item")
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(sItems) {
+
+            }
+        }
+    }
+    if (showDialog)    {
+        AlertDialog(onDismissRequest = { /*TODO*/ }, confirmButton = { /*TODO*/ })
+    }
+  }
