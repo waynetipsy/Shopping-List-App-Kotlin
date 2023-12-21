5300e3118776462ad325dab42c11207e77d0cb92
waynetipsy <jacketoniru@gmail.com>
2023-12-21 22:04:10 +0100

    appUI

--- app/src/main/java/app/ify/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/app/ify/myshoppinglistapp/ShoppingList.kt
@@ -2,12 +2,16 @@ package app.ify.myshoppinglistapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,16 +30,19 @@ data class ShoppingItem(
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
   fun ShoppingListApp () {
     var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
     var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
 
     Column (modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ){
         Button(
-            onClick = { },
+            onClick = { showDialog = true },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text( "Add Item")
@@ -51,6 +58,48 @@ data class ShoppingItem(
         }
     }
     if (showDialog)    {
-        AlertDialog(onDismissRequest = { /*TODO*/ }, confirmButton = { /*TODO*/ })
+        AlertDialog(onDismissRequest = { showDialog = false },
+            confirmButton = {
+                            Row (modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                           horizontalArrangement = Arrangement.SpaceBetween) {
+                               Button(onClick = {
+                                   if (itemName.isNotBlank()){
+                                       val newItem = ShoppingItem(
+                                           id = sItems.size+1,
+                                       )
+                                   }
+                               }) {
+                                  Text( "Add")
+                               }
+                                Button(onClick = { showDialog = false }) {
+                                    Text( "Cancel")
+                                }
+                            }
+                            } ,
+            title = { Text("Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = {itemName = it },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = {itemQuantity = it },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
+            }
+        )
+
     }
   }
