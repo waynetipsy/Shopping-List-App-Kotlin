7e63e47f110136ef9ce321602a3c17ae0ce686cd
waynetipsy <jacketoniru@gmail.com>
2023-12-25 20:19:06 +0100

    composable

--- app/src/main/java/app/ify/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/app/ify/myshoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package app.ify.myshoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -53,7 +57,7 @@ data class ShoppingItem(
                 .padding(16.dp)
         ) {
             items(sItems) {
-
+            ShoppingListItem(it,{},{})
             }
         }
     }
@@ -68,7 +72,12 @@ data class ShoppingItem(
                                    if (itemName.isNotBlank()){
                                        val newItem = ShoppingItem(
                                            id = sItems.size+1,
+                                           name = itemName,
+                                           quantity = itemQuantity.toInt()
                                        )
+                                       sItems = sItems + newItem
+                                       showDialog = false
+                                       itemName = ""
                                    }
                                }) {
                                   Text( "Add")
@@ -103,3 +112,22 @@ data class ShoppingItem(
 
     }
   }
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Row (modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()
+        .border(
+            border = BorderStroke(2.dp, Color(0XFF018786)),
+            shape = RoundedCornerShape(20)
+        )
+    ){
+       Text(text = item.name, modifier = Modifier.padding(8.dp))
+    }
+
+}
